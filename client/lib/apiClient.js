69912e3a2cd0589223022e1f8c22dfd9ee98a779
Swdhss0 <swad0100@gmail.com>
2024-09-06 11:15:39 +0900

    commit

--- client/lib/apiClient.js
+++ client/lib/apiClient.js
@@ -2,7 +2,8 @@ import axios from "axios";
 
 
 const apiClient = axios.create({
-  baseURL: "http://localhost:8181/api/",
+  // baseURL: "http://localhost:8181/api/",
+  baseURL: process.env.BACKEND_URL,
   headers: {
     'Content-type': 'Application/json',
   },
